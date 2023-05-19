2c4fc7fad3cd8b15d0919bf6d3446d3cb4b1cc7a
Sarahh1928 <119070393+Sarahh1928@users.noreply.github.com>
2023-05-19 13:35:19 +0300

    Add files via upload

--- jfile.js
+++ jfile.js
@@ -1,7 +1,45 @@
 function toggleSection(sectionId) {
   var elements = document.getElementsByClassName("sections");
   
-  console.log(sectionId);
+  for (var i = 0; i < elements.length; i++) {
+    if (elements[i].id === sectionId) {
+      elements[i].classList.remove("hidden");
+    } else {
+      elements[i].classList.add("hidden");
+    }
+  }
+}
+const inputBox = document.getElementById('inputBox');
+const myButton = document.getElementById('myButton');
+
+inputBox.addEventListener('input', function() {
+  if (inputBox.value !== '') {
+    myButton.classList.add('active');
+  } else {
+    myButton.classList.remove('active');
+  }
+});
+
+
+document.addEventListener('DOMContentLoaded', function() {
+  const inputBox = document.getElementById('inputBox');
+  const myButton = document.getElementById('myButton');
+  const searchResults = document.getElementById('hlist');
+
+  myButton.addEventListener('click', function(event) {
+
+    // Retrieve the value from the search input
+    const searchText = inputBox.value.trim();
+
+    // Check if the search text is equal to "mohamed"
+    if (searchText.toLowerCase() === 'mohamed') {
+      searchResults.classList.remove('hidden'); // Show the search results
+    }
+  });
+});
+
+function viewd(sectionId) {
+  var elements = document.getElementsByClassName("details");
   
   for (var i = 0; i < elements.length; i++) {
     if (elements[i].id === sectionId) {
@@ -11,3 +49,4 @@ function toggleSection(sectionId) {
     }
   }
 }
+

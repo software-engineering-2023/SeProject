d1a2c328737a314601dc0a0e94b9118778943361
mennan0809 <121335355+mennan0809@users.noreply.github.com>
2023-05-20 03:37:34 +0300

    Recaptcha in homePage
    Added recaptcha verification to inquiries submission

--- app.js
+++ app.js
@@ -54,14 +54,53 @@ const msg = document.createElement("p");
 msg.setAttribute("id", "validation");
 msg.innerHTML = "Email sent successfully!";
 
-form.addEventListener("submit", function (event) {
-  if (!form.checkValidity()) {
-    event.preventDefault(); // Prevent form submission if validation fails
-  } else {
-    form.reset();
-    form.appendChild(msg);
+var captcha;
+function generate() {
+  // Clear old input
+  document.getElementById("captcha").value = "";
+
+  // Access the element to store
+  // the generated captcha
+  captcha = document.getElementById("captchaCode");
+  var uniquechar = "";
+
+  const randomchar =
+    "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789";
+
+  // Generate captcha for length of
+  // 5 with random character
+  for (let i = 1; i < 5; i++) {
+    uniquechar += randomchar.charAt(Math.random() * randomchar.length);
   }
-  setTimeout(function () {
-    form.removeChild(msg); // Remove the message after 5 seconds
-  }, 4000);
+
+  // Store generated input
+  captcha.innerHTML = uniquechar;
+}
+form.addEventListener("submit", function (event) {
+  const user_input = document.getElementById("captcha").value;
+  console.log("hello");
+  console.log(user_input);
+  console.log(captcha.innerHTML);
+  if (user_input == captcha.innerHTML) {
+    console.log("hello22");
+    if (!form.checkValidity()) {
+      event.preventDefault(); // Prevent form submission if validation fails
+    } else {
+      event.preventDefault();
+      form.reset();
+      msg.innerHTML = "Email sent successfully!";
+      form.appendChild(msg);
+      setTimeout(function () {
+        form.removeChild(msg); // Remove the message after 5 seconds
+      }, 4000);
+    }
+  } else {
+    event.preventDefault();
+    msg.innerHTML = "Incorrect Captcha Code";
+    form.appendChild(msg);
+    generate();
+    setTimeout(function () {
+      form.removeChild(msg); // Remove the message after 5 seconds
+    }, 4000);
+  }
 });

0e10d3531cc18ab058eb2aa198c1e576fe7bddf4
Sarahh1928 <119070393+Sarahh1928@users.noreply.github.com>
2023-05-26 15:35:34 +0300

    Add files via upload

--- jfile.js
+++ jfile.js
@@ -1,6 +1,6 @@
 function toggleSection(sectionId) {
   var elements = document.getElementsByClassName("sections");
-  
+
   for (var i = 0; i < elements.length; i++) {
     if (elements[i].id === sectionId) {
       elements[i].classList.remove("hidden");
@@ -12,7 +12,7 @@ function toggleSection(sectionId) {
 const inputBox = document.getElementById('inputBox');
 const myButton = document.getElementById('myButton');
 
-inputBox.addEventListener('input', function() {
+inputBox.addEventListener('input', function () {
   if (inputBox.value !== '') {
     myButton.classList.add('active');
   } else {
@@ -21,17 +21,17 @@ inputBox.addEventListener('input', function() {
 });
 
 
-document.addEventListener('DOMContentLoaded', function() {
+document.addEventListener('DOMContentLoaded', function () {
   const inputBox = document.getElementById('inputBox');
   const myButton = document.getElementById('myButton');
   const searchResults = document.getElementById('hlist');
 
-  myButton.addEventListener('click', function(event) {
+  myButton.addEventListener('click', function (event) {
 
     // Retrieve the value from the search input
     const searchText = inputBox.value.trim();
     let substrings = [];
-    word="mohamed";
+    word = "mohamed";
     for (let i = 0; i < word.length; i++) {
       for (let j = i + 1; j <= word.length; j++) {
         let substring = word.slice(i, j);
@@ -42,15 +42,15 @@ document.addEventListener('DOMContentLoaded', function() {
     if (substrings.includes(searchText)) {
       searchResults.classList.remove('hidden'); // Show the search results
     }
-    else{
-      searchResults.classList.add('hidden'); 
+    else {
+      searchResults.classList.add('hidden');
     }
   });
 });
 
 function viewd(sectionId) {
   var elements = document.getElementsByClassName("details");
-  
+
   for (var i = 0; i < elements.length; i++) {
     if (elements[i].id === sectionId) {
       elements[i].classList.remove("hidden");
@@ -63,7 +63,7 @@ function viewd(sectionId) {
 const reply1 = document.getElementById('reply1');
 const submit1 = document.getElementById('submit1');
 
-reply1.addEventListener('input', function() {
+reply1.addEventListener('input', function () {
   if (reply1.value !== '') {
     submit1.classList.add('active');
   } else {
@@ -72,10 +72,10 @@ reply1.addEventListener('input', function() {
 });
 const question1 = document.getElementById('question1');
 
-submit1.addEventListener('click', function(event) {
-  event.preventDefault(); 
+submit1.addEventListener('click', function (event) {
+  event.preventDefault();
   if (reply1.value !== '') {
-  question1.classList.add('hidden');
+    question1.classList.add('hidden');
   }
 });
 
@@ -83,7 +83,7 @@ submit1.addEventListener('click', function(event) {
 const reply2 = document.getElementById('reply2');
 const submit2 = document.getElementById('submit2');
 
-reply2.addEventListener('input', function() {
+reply2.addEventListener('input', function () {
   if (reply2.value !== '') {
     submit2.classList.add('active');
   } else {
@@ -92,8 +92,8 @@ reply2.addEventListener('input', function() {
 });
 const question2 = document.getElementById('question1');
 
-submit2.addEventListener('click', function(event) {
-  event.preventDefault(); 
+submit2.addEventListener('click', function (event) {
+  event.preventDefault();
   if (reply2.value !== '') {
     question2.classList.add('hidden');
   }
@@ -103,7 +103,7 @@ const email1 = document.getElementById('email');
 const password1 = document.getElementById('password');
 const savechanges = document.getElementById('savechanges');
 
-name1.addEventListener('input', function() {
+name1.addEventListener('input', function () {
   if (name1.value !== '' && email1.value !== '' && password1.value !== '') {
     savechanges.classList.add('active');
   } else {
@@ -111,7 +111,7 @@ name1.addEventListener('input', function() {
   }
 });
 
-email1.addEventListener('input', function() {
+email1.addEventListener('input', function () {
   if (name1.value !== '' && email1.value !== '' && password1.value !== '') {
     savechanges.classList.add('active');
   } else {
@@ -119,10 +119,94 @@ email1.addEventListener('input', function() {
   }
 });
 
-password1.addEventListener('input', function() {
+password1.addEventListener('input', function () {
   if (name1.value !== '' && email1.value !== '' && password1.value !== '') {
     savechanges.classList.add('active');
   } else {
     savechanges.classList.remove('active');
   }
 });
+// Function to increment the counter
+// function incrementCounter() {
+//   var counter1Element = document.getElementById("counter");
+//   var counter1Value = parseInt(counter1Element.innerHTML);
+//   var counter2Element = document.getElementById("counterr");
+//   var counter2Value = parseInt(counter2Element.innerHTML);
+//   var counter3Element = document.getElementById("counterrr");
+//   var counter3Value = parseInt(counter3Element.innerHTML);
+//   var counter4Element = document.getElementById("counterrrr");
+//   var counter4Value = parseInt(counter4Element.innerHTML);
+//   if (counter1Value < 265 || counter2Value < 120 || counter3Value < 356780 || counter4Value < 89) {
+
+//     if (counter1Element < 265) {
+//       counter1Value += 1;
+//       counter1Element.innerHTML = counter1Value;
+//     }
+//     if (counter2Element < 120) {
+//       counter2Value += 1;
+//       counter2Element.innerHTML = counter2Value;
+//     }
+//     if (counter3Element < 356780) {
+//       counter3Value += 1;
+//       counter3Element.innerHTML = "$" + counter3Value;
+//     }
+//     if (counter4Element < 89) {
+//       counter4Value += 1;
+//       counter4Element.innerHTML = counter4Value + "%";
+//     }
+
+//     setTimeout(incrementCounter, 2);
+//   }
+// }
+
+// // Start the counter when the page loads
+// window.onload = function () {
+//   incrementCounter();
+// };
+
+function increaseNumber(id, maxNumber) {
+  var element = document.getElementById(id);
+  var currentNumber = parseInt(element.innerText);
+  
+  // Increase the number every 100 milliseconds
+  if(maxNumber===89){
+  var interval = setInterval(function() {
+    currentNumber++;
+    element.innerText = currentNumber+"%";
+
+    if (currentNumber >= maxNumber) {
+      clearInterval(interval);
+    }
+  }, 2);
+ }
+ else if(maxNumber===2000|maxNumber===2500|maxNumber===3000){
+ var interval = setInterval(function() {
+   currentNumber++;
+   element.innerText ="$"+ currentNumber;
+
+   if (currentNumber >= maxNumber) {
+     clearInterval(interval);
+   }
+ }, 2);
+}
+else{
+var interval = setInterval(function() {
+  currentNumber++;
+  element.innerText = currentNumber;
+
+  if (currentNumber >= maxNumber) {
+    clearInterval(interval);
+  }
+}, 2);
+}
+}
+
+increaseNumber("counter", 265);   
+increaseNumber("counterr", 120);    
+increaseNumber("counterrr", 2000);    
+increaseNumber("counterrrr", 89);    
+increaseNumber("counter1", 2500);    
+increaseNumber("counter2", 1000);     
+increaseNumber("counter3", 3);    
+increaseNumber("counter4", 3000);    
+

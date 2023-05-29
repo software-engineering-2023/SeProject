e896dd8e2eb01e4683dfd24ec2f6ada7089018b2
Ahmedwalida <127797118+Ahmedwalida@users.noreply.github.com>
2023-05-29 20:40:53 +0300

    Update apply for a credit card.js

--- apply for a credit card.js
+++ apply for a credit card.js
@@ -1,22 +1,17 @@
 function submitCreditCardApplication(event) {
   event.preventDefault();
 
-  // Get form input values
   var fullName = document.getElementById('name').value;
   var email = document.getElementById('email').value;
   var phoneNumber = document.getElementById('phone').value;
   var annualIncome = document.getElementById('income').value;
-
-  // Perform any additional validation or processing here
   
-  // Display the submitted values
   console.log('Credit Card Application Details:');
   console.log('Full Name:', fullName);
   console.log('Email:', email);
   console.log('Phone Number:', phoneNumber);
   console.log('Annual Income:', annualIncome);
 
-  // Reset the form
   document.getElementById('creditCardApplicationForm').reset();
 }
 
